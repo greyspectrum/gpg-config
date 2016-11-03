4e0b61d89d79fe9fe03d086ac42b491fa96cbf2c
greyspectrum <greyspectrum@users.noreply.github.com>
2016-11-03 14:51:50 -0400

    remove some things

--- gpg-config.sh
+++ gpg-config.sh
@@ -3,7 +3,7 @@
 ##############################################################################
 # gpg-config
 # -----------
-# Creates gpg keys for you with sane defaults.
+# Creates gpg keys with sane defaults.
 #  
 # :author: greyspectrum
 # :date: 3 November 2016
@@ -12,39 +12,8 @@
 
 # Define variables 
 
-GPG_CONF="/.gnupg/gpg.conf"
+GPG_CONF=".gnupg/gpg.conf"
 
-BACKUP_GPG_CONF="/.gnupg/gpgconf.bak"
+BACKUP_GPG_CONF=".gnupg/gpgconf.bak"
 
-# Prompt the user for root
 
-[ "$(whoami)" != "root" ] && exec sudo -- "$0" "$@"
-
-# Test if GPG is installed.
-
-if test -e $GPG_CONF ; then
-	gpg --version
-else
-	while true; do
-		read -p "GPG isn't installed, install it?" yn
-		case $yn in
-			[Yy]* ) read -p "Do you want ECC keys (recommended)?" yn
-				case $yn in
-					[Yy]* ) apt-get install gpg2
-						
-								    ; break ;;
-					[Nn]* ) apt-get install gpg 
-						gpg --gen-key
-						echo "1" ^M
-						echo "4096" ^M 
- 						echo "1y" ^M
-						echo "y" ^M
-						read -p #pick up where you left off
-								    ; break ;;
-					* )	echo "Please answer y (yes) or n (no)." ;;
-				esac	
-			[Nn]* ) echo "WTF. Why are you running this script, then?" && exit ;;
-			* ) 	echo "Please answer y (yes) or n (no)." ;;
-		esac
-	done
-fi
